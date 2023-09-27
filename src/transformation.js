bcf1573b87f6b40350c111e812a9ec51b8370385
klmork <klmork@calpoly.edu>
2023-09-26 19:19:55 -0700

    Refactor: Pull Miles Into Object
    Pull miles code into its own object.
    
    The base code was used in a demo before students had learned about variables and functions.  We don't need to worry about that here, so can add variables for location, etc.
    
    The delta variables aren't used yet, but will be in the next commit when adding the animations.

--- src/transformation.js
+++ src/transformation.js
@@ -1,31 +1,27 @@
-
-
-function setup() 
-{
-   createCanvas(400, 400);
-   background(200);
-}
-
-function draw() {
-  // box - okay if background is not at origin
-  fill(128, 60, 32);
-  rect(150 , 300, 100, 100);
-
-  //rect()
-  push();
-    translate(200, 300-80);
+const miles = {
+  x: 200,
+  y: 300,
+  scaling: 1,
+  deltaScaling: 0.01,
+  rotation: 0,
+  deltaRotation: 0.01,
+  drawMiles: function () {
+    push();
+    translate(this.x, this.y - 80);
+    rotate(this.rotation);
+    scale(this.scaling);
     //draw head
     fill(0);
-    ellipse(0,-75,60,75);
+    ellipse(0, -75, 60, 75);
     //eye ring
-    fill(255,0,0);
+    fill(255, 0, 0);
     ellipse(-15, -80, 22, 17);
     ellipse(15, -80, 22, 17);
     //eyes
     fill(255);
     ellipse(-15, -80, 18, 13);
     ellipse(15, -80, 18, 13);
-   
+
     //arms
     fill(0);
     ellipse(-40, -25, 50, 15);
@@ -33,37 +29,37 @@ function draw() {
     //hands
     ellipse(-60, -25, 15, 15);
     ellipse(60, -25, 15, 15);
-   
+
     //body miles
     fill(0);
-    ellipse(0,0,50,75);   
+    ellipse(0, 0, 50, 75);
     //spider logo
-    fill(255,0,0);
-    ellipse(0,-15,10,15);
-    stroke(255,0,0);
+    fill(255, 0, 0);
+    ellipse(0, -15, 10, 15);
+    stroke(255, 0, 0);
     //top
-    line(-15,-25,0,-20);
-    line(15,-25,0,-20);
+    line(-15, -25, 0, -20);
+    line(15, -25, 0, -20);
     //top-middle
-    line(-15,-20,0,-20);
-    line(15,-20,0,-20);
+    line(-15, -20, 0, -20);
+    line(15, -20, 0, -20);
     //bottom
-    line(-15,-15,0,-15);
-    line(15,-15,0,-15);
+    line(-15, -15, 0, -15);
+    line(15, -15, 0, -15);
     //bottom-middle
-    line(-15,-10,0,-15);
-    line(15,-10,0,-15);
-   
+    line(-15, -10, 0, -15);
+    line(15, -10, 0, -15);
+
     noStroke();
-   
-     //feet
+
+    //feet
     fill(0);
-    ellipse(-15,75,20,10); //bottom of feet: 75 + 5
-    ellipse(15,75,20,10);
+    ellipse(-15, 75, 20, 10); //bottom of feet: 75 + 5
+    ellipse(15, 75, 20, 10);
     //legs
     fill(0);
-    ellipse(-10,50,20,50);
-    ellipse(10,50,20,50);
+    ellipse(-10, 50, 20, 50);
+    ellipse(10, 50, 20, 50);
 
     // show origin
     fill(0, 255, 255);
@@ -72,10 +68,18 @@ function draw() {
     line(-100, 0, 100, 0);
     stroke(0, 0, 255);
     line(0, 100, 0, -100);
-  pop();
+    pop();
+  },
+};
 
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+  miles.drawMiles();
 }
 
-
-
-
+function draw() {
+  // box - okay if background is not at origin
+  fill(128, 60, 32);
+  rect(150, 300, 100, 100);
+}
